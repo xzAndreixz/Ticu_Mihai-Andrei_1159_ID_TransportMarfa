906714249b97a9da24ecb2bd71828313bcdd025c
xzAndreixz <ticumihaiandrei27@gmail.com>
2025-10-17 21:36:18 +0300

    Alegere clase si declarare variabile conform cerintei

--- TransportMarfa/app.cpp
+++ TransportMarfa/app.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
+class Camion {
+public:
+	int vitezaMax;
+	float* consum;
+	char* marca;
+	const int nrRoti;
+	static int nrCamioane;
+};
+
+class Sofer {
+	char* nume;
+	float* aniExp;
+	int varsta;
+	const char* catPermis;
+	static int nrSoferi;
+
+};
+
+class Remorca {
+	float* capacitate;
+	const int nrAxe;
+	bool franaProprie;
+	const char* tipRemorca;
+	static int nrRemorci;
+};
 
 int main() {
 
