04efdcb9af064a9b9028ce7e59b84aaf8f104741
xzAndreixz <ticumihaiandrei27@gmail.com>
2025-10-18 20:29:55 +0300

    Initializarea atributelor statice!

--- TransportMarfa/app.cpp
+++ TransportMarfa/app.cpp
@@ -9,24 +9,42 @@ public:
 	char* marca;
 	const int nrRoti;
 	static int nrCamioane;
+
+	Camion() : nrRoti(6){ //declari aici fiindca const trebuie declarat inainte de crearea obiectului altfel ar da eroare...nu poti modifica o constanta dupa crearea obiectului!
+		vitezaMax = 90;
+	}
 };
 
+int Camion::nrCamioane = 0;
+
 class Sofer {
+public:
 	char* nume;
 	float* aniExp;
 	int varsta;
 	const char* catPermis;
 	static int nrSoferi;
 
+	Sofer() : catPermis("CE") {
+		nrSoferi++;
+	}
+
 };
+int Sofer::nrSoferi = 0;
 
 class Remorca {
+public:
 	float* capacitate;
 	const int nrAxe;
 	bool franaProprie;
 	const char* tipRemorca;
 	static int nrRemorci;
+
+	Remorca() : capacitate(new float(10.0f)),nrAxe(2),franaProprie(true), tipRemorca("Prelata") {
+		nrRemorci++;
+	}
 };
+int Remorca::nrRemorci = 0;
 
 int main() {
 
