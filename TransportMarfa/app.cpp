7553196cb6984af19447f5787f2349a16bcf148e
xzAndreixz <ticumihaiandrei27@gmail.com>
2025-10-20 15:38:16 +0300

    Exersare transmitere variabila prin valoare, pointer, referinta. Descoperire si remediere leak(vezi comentarii rand: 102-105)

--- TransportMarfa/app.cpp
+++ TransportMarfa/app.cpp
@@ -57,17 +57,67 @@ public:
 };
 int Remorca::nrRemorci = 0;
 
+int transmiterePrinPointer(int a, int b,int* s) {
+	*s = a + b;
+	return *s;
+}
+
+void afisare(int parametru) {
+	cout << "Valoarea afisata este: " << parametru;
+}
+
+void sumaPrinRefereinta(int a, int b, int& s2) {
+	s2 = a + b;
+}
+
+void inmultesteValoare(float* valoare, int deCateOri) {
+	for (int i = 0; i < deCateOri; i++)
+		(*valoare) = (*valoare) * 2;
+	cout << "Valoarea noua este: " << *valoare;
+}
+
+void dubleaza(float** valoare) {
+	(**valoare) = (**valoare) * (**valoare);
+	cout << "Valoarea dubla este: " << **valoare;
+}
+
 int main() {
 
-	Camion c1;
+	int s = 0, s2 = 0, a = 10;
+	float b=56.0;
 
-	cout << "Introduceti marca: ";
-	cin >> c1.marca;
+	cout <<"\nSuma este:"<< transmiterePrinPointer(5, 7, &s);  //am pus &s fiindca functia asteapta o adresa
+	cout << endl;
+	afisare(a);
+	cout << endl;
+	sumaPrinRefereinta(15, 21, s2);
+	cout << endl;
+	afisare(s2);
+	cout << endl;
+
+	//daca ai valori alocate dinamic?
+
+	float* p;
+
+	p = new float; //am alocat mem. in heap :)
+	delete p;  //eliberam memoria cand nu mai folosim
+	p = &b; //aici am retinut adresa lui b ...am eliberat mai sus fiindca daca retineam pentru b si nu elibeream cream un leak(mem. blocata)
+			//asadar acea memorie rezervata cu "new float" am eliberato cu delete.
+	cout <<"Adresa este:"<<p<<", dar valoarea este: "<<*p;
+	cout << endl;
+	float val = 5;
+	inmultesteValoare(&val, 3);  //functia inmulteste cu 2 si modifica var. globara de cate ori ii dai ca parametru prin int
+	//sa folosim si *
+	cout << endl;
+	inmultesteValoare(p, 3);
+	cout << endl;
+	dubleaza(&p);  //aici folosesc ** unde prima * dereferentiaza catre valoarea din p adica adresa lui b si a doua steluta dereferentiaza pentru a afla valoarea lui b
 
 
-	cout << c1.marca;
+	//si astfel am exersat si inteles cele 3 tipuri prin care poti trimite o variabila in C++: valoare, pointer, referinta
 
-
+	
+	cout << b;
 	return 0;
 
 }
