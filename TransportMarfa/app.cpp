6fa36c5a3eabb3e37b6fde70192eb9e7a87867f3
xzAndreixz <ticumihaiandrei27@gmail.com>
2025-10-27 19:37:24 +0200

    Am creat un constructor cu parametri partiali pentru clasa camion, acum testez!

--- TransportMarfa/app.cpp
+++ TransportMarfa/app.cpp
@@ -11,10 +11,17 @@ public:
 	static int nrCamioane;
 
 	Camion() : nrRoti(6){ //declari aici fiindca const trebuie declarat inainte de crearea obiectului altfel ar da eroare...nu poti modifica o constanta dupa crearea obiectului!
-		vitezaMax = 90;
-		consum = new float(38.5f);
-		marca = new char[15]; //aloc 15 caractere pentru marca de tir
-		strcpy_s(marca, 15, "Nedeclarata");
+		this->vitezaMax = 90;
+		this->consum = new float(38.5f);
+		this->marca = new char[15]; //aloc 15 caractere pentru marca de tir
+		strcpy_s(this->marca, 15, "Nedeclarata");
+
+	}
+	Camion(float consum, const char* marca): nrRoti(6) {
+		this->vitezaMax = 90;
+		this->consum = new float(consum);
+		this->marca = new char[strlen(marca) + 1];
+		strcpy_s(this->marca, strlen(marca) + 1, marca);
 
 	}
 
@@ -54,70 +61,23 @@ public:
 		nrRemorci++;
 
 	}
+
+
 };
 int Remorca::nrRemorci = 0;
 
-int transmiterePrinPointer(int a, int b,int* s) {
-	*s = a + b;
-	return *s;
-}
 
-void afisare(int parametru) {
-	cout << "Valoarea afisata este: " << parametru;
-}
 
-void sumaPrinRefereinta(int a, int b, int& s2) {
-	s2 = a + b;
-}
 
-void inmultesteValoare(float* valoare, int deCateOri) {
-	for (int i = 0; i < deCateOri; i++)
-		(*valoare) = (*valoare) * 2;
-	cout << "Valoarea noua este: " << *valoare;
-}
-
-void dubleaza(float** valoare) {
-	(**valoare) = (**valoare) * (**valoare);
-	cout << "Valoarea dubla este: " << **valoare;
-}
 
 int main() {
 
-	int s = 0, s2 = 0, a = 10;
-	float b=56.0;
+	Camion man01(58.5, "Man Tip 1");
 
-	cout <<"\nSuma este:"<< transmiterePrinPointer(5, 7, &s);  //am pus &s fiindca functia asteapta o adresa
-	cout << endl;
-	afisare(a);
-	cout << endl;
-	sumaPrinRefereinta(15, 21, s2);
-	cout << endl;
-	afisare(s2);
-	cout << endl;
-
-	//daca ai valori alocate dinamic?
-
-	float* p;
-
-	p = new float; //am alocat mem. in heap :)
-	delete p;  //eliberam memoria cand nu mai folosim
-	p = &b; //aici am retinut adresa lui b ...am eliberat mai sus fiindca daca retineam pentru b si nu elibeream cream un leak(mem. blocata)
-			//asadar acea memorie rezervata cu "new float" am eliberato cu delete.
-	cout <<"Adresa este:"<<p<<", dar valoarea este: "<<*p;
-	cout << endl;
-	float val = 5;
-	inmultesteValoare(&val, 3);  //functia inmulteste cu 2 si modifica var. globara de cate ori ii dai ca parametru prin int
-	//sa folosim si *
-	cout << endl;
-	inmultesteValoare(p, 3);
-	cout << endl;
-	dubleaza(&p);  //aici folosesc ** unde prima * dereferentiaza catre valoarea din p adica adresa lui b si a doua steluta dereferentiaza pentru a afla valoarea lui b
-
-
-	//si astfel am exersat si inteles cele 3 tipuri prin care poti trimite o variabila in C++: valoare, pointer, referinta
-
+	cout << man01.marca<<endl;
+	cout << man01.consum;
 	
-	cout << b;
+
 	return 0;
 
 }
