74533a1c374406410a9a185d0c632a679a6e6ea7
xzAndreixz <ticumihaiandrei27@gmail.com>
2025-11-19 20:41:58 +0200

    Add files via upload
    Exersare

---
+++ LucruPersonal/app.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std; //pune explicatie pentru cin, cout, endl, etc...mai usor ca scriere! 
+
+class laptop {
+	//clasa are implicit sectiune private daca nu declari niciuna...sectiune este corect exprimat?
+public:
+	
+private:
+	char* model;
+	char* tipStocare;
+	float* stocare;
+	static int nrLaptopuri;  //cum intializezi?
+	char* procesor;
+	int ram;
+	char* placaVideo;
+
+
+};
+
+
+int main() {  //int main cand vrei sa apelezi in alta aplicatie
+
+
+
+	return 0;
+}
